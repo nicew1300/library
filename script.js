200ebc6177a5262e25b2e107a3d752c28063972b
Luka Stromajer <str240266@spengergasse.at>
2026-02-21 14:13:34 +0100

    Add book form and modal styling for user input, ready for status and remove button update

--- script.js
+++ script.js
@@ -1,33 +1,75 @@
-function Book(title, author, year) {
-    this.title = title;
-    this.author = author;
-    this.year = year;
-}
+const myLibrary = []
+const addBtn = document.querySelector(".add")
+const modal = document.querySelector("dialog")
+const content = document.querySelector(".content")
 
-function createBook() {
-    let title = document.getElementById('title').value;
-    let author = document.getElementById('author').value;
-    let year = document.getElementById('year').value;
+addBtn.addEventListener("click", () => {
+    modal.showModal()
+})
 
-    let book = new Book(title, author, year);
-    console.log(book);
-}
-
-// Make modal popup when button is clicked
-let modal = document.getElementById('myModal');
-let btn = document.getElementById('myBtn');
-let span = document.getElementsByClassName('close')[0];
-
-btn.onclick = function() {
-    modal.style.display = 'block';
-}
-
-span.onclick = function() {
-    modal.style.display = 'none';
-}
-
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = 'none';
+// close the modal if clicked outside, i got this from web dev simplified dialog element tutorial
+modal.addEventListener("click", (e) => {
+    const dialogDimensions = modal.getBoundingClientRect();
+    if (
+        e.clientX < dialogDimensions.left ||
+        e.clientX > dialogDimensions.right ||
+        e.clientY < dialogDimensions.top ||
+        e.clientY > dialogDimensions.bottom
+    ) {
+        modal.close();
     }
+});
+
+const form = document.querySelector("form")
+
+form.addEventListener("submit", createBook)
+
+function Book(titleInput, authorInput, pagesInput) {
+    this.title = titleInput;
+    this.author = authorInput;
+    this.pages = pagesInput;
+    this.id = crypto.randomUUID()
+}
+
+
+function createBook(e) {
+    // prevent page refresh
+    e.preventDefault();
+
+    const titleInput = document.querySelector('#title').value;
+    const authorInput = document.querySelector('#author').value;
+    const pagesInput = document.querySelector('#pages').value;
+
+    const book = new Book(titleInput, authorInput, pagesInput);
+    myLibrary.push(book)
+    
+    displayBook(book)
+ 
+    document.querySelector('form').reset(); // Clears the inputs
+    modal.close();
+
+}
+
+function displayBook(book) {
+    const bookDiv = document.createElement("div")
+    bookDiv.classList.add("book")
+
+    const bookTitleDiv = document.createElement("div")
+    bookTitleDiv.classList.add("book-title")
+    bookTitleDiv.textContent = book.title
+    bookDiv.appendChild(bookTitleDiv)
+
+    const bookAuthorDiv = document.createElement("div")
+    bookAuthorDiv.classList.add("book-author")
+    bookAuthorDiv.textContent = `by ${book.author}`
+    bookDiv.appendChild(bookAuthorDiv)
+
+    const bookPagesDiv = document.createElement("div")
+    bookPagesDiv.classList.add("book-pages")
+    bookPagesDiv.textContent = `${book.pages} pages`
+    bookDiv.appendChild(bookPagesDiv)
+
+    content.appendChild(bookDiv)
+
+    console.log(myLibrary)
 }
